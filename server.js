0c5f423ab26a88059dd423644f445557f2d850da
MORONFOLUWA <moronfoluwaakintola@gmail.com>
2021-09-26 01:14:41 +0100

    I imported auth and todos routes

--- server.js
+++ server.js
@@ -3,9 +3,14 @@ const app = express();
 const cors = require("cors");
 
 //middleware
-
 app.use(cors());
 app.use(express.json());
+
+//routes
+app.use("/authentication", require("./routes/auth"));
+app.use("/dashboard", require("./routes/todos"));
+
+
 app.listen(5000, () => {
   console.log(`Server is starting on port 5000`);
 });
